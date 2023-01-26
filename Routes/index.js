11cc6722ab62be769ecd75e5d3485783b78bec00
Afan Rais <afanrais@MacBook-Air-Afan.local>
2023-01-27 01:33:04 +0700

    init appoinment route

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router()
 
 const User = require('./User.Router')
+const Appoinment = require('./Appoinment.Router')
 
 Router.use('/user', User)
+Router.use('/appoinment', Appoinment)
 
 module.exports = Router
