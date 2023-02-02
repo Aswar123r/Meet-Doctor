5658c306b4c0c8898bf1e849da65725956e7ebd6
Afan Rais <afanrais@MacBook-Air-Afan.local>
2023-02-02 19:29:08 +0700

    delete unused file

--- Routes/Appoinment.Router.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express").Router();
-
-const Appoinment = require("../Controllers/Appoinment.controller");
-
-Router.post("/save", Appoinment.Appoinment);
-
-module.exports = Router;
