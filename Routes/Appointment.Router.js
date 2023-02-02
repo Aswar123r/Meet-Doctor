baf468ba03fe8196a274b2d63e42d625baa5f093
Afan Rais <afanrais@MacBook-Air-Afan.local>
2023-02-02 20:00:28 +0700

    add auth in appointment controller

--- Routes/Appointment.Router.js
+++ Routes/Appointment.Router.js
@@ -1,9 +1,14 @@
 const Router = require("express").Router();
 
 const Appointment = require("../Controllers/Appointment.controller");
+const Auth = require("../Middleweres/Auth.middleware");
 
+Router.use(Auth);
 Router.post("/save", Appointment.SaveAppointment);
-Router.get("/get/patient/:patientId", Appointment.GetAllAppointmentsByPatientId)
-Router.get("/get/doctor/:doctorId", Appointment.GetAllAppointmentsByDoctorId)
+Router.get(
+  "/get/patient/",
+  Appointment.GetAllAppointmentsByPatientId
+);
+Router.get("/get/doctor/", Appointment.GetAllAppointmentsByDoctorId);
 
 module.exports = Router;
